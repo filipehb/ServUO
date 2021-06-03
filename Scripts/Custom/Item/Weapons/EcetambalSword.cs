762ea1aeb487ce6883127ca6cb960540c6fa5ebd
Filipe Arruda <filipehenriquester@gmail.com>
2021-06-02 22:46:35 -0300

    Criada espada tipo excalibur

---
+++ Scripts/Custom/Item/Weapons/EcetambalSword.cs
@@ -0,0 +1,50 @@
+﻿// **********
+// ServUO - EcetambalSword.cs
+// **********
+
+using Server.Engines.Craft;
+
+namespace Server.Items
+{
+	[Alterable(typeof(DefBlacksmithy), typeof(StoneWarSword))]
+	[Flipable(0x13B9, 0x13Ba)]
+	public class EcetambalSword : BaseSword //Essa espada deve ficar preso em uma pedra feito a excalibur
+	{
+		[Constructable]
+		public EcetambalSword()
+			: base(0x13B9)
+		{
+			Weight = 6.0;
+			Hue = 1; //TODO Alterar para uma cor legal
+			Light = LightType.Moongate;
+		}
+
+		public EcetambalSword(Serial serial)
+			: base(serial)
+		{
+		}
+		
+		public override WeaponAbility PrimaryAbility => WeaponAbility.CrushingBlow;
+		public override WeaponAbility SecondaryAbility => WeaponAbility.ParalyzingBlow;
+		public override int StrengthReq => 40;
+		public override int MinDamage => 35;
+		public override int MaxDamage => 49;
+		public override float Speed => 2.75f;
+
+		public override int DefHitSound => 0x237;
+		public override int DefMissSound => 0x23A;
+		public override int InitMinHits => 100;
+		public override int InitMaxHits => 200;
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write(0); // version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+			int version = reader.ReadInt();
+		}
+	}
+}
